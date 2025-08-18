856907751b10b6eff6336ed96ded86091d467c53
Hessel <vgerlachhessel@gmail.com>
2025-08-18 16:43:14 -0300

    initial structure using JDK 21, maven and spring

---
+++ src/main/java/com/eventcore/EventcoreApplication.java
@@ -0,0 +1,13 @@
+package com.eventcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventcore/EventcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
