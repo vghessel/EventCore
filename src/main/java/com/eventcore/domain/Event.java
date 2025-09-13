62a24d5bd150cc34634ba983ca9fef504f054518
hessel <vgerlachhessel@gmail.com>
2025-09-13 16:15:53 -0300

    fix: docker-compose file and adjust in event entity

--- src/main/java/com/eventcore/domain/Event.java
+++ src/main/java/com/eventcore/domain/Event.java
@@ -4,10 +4,6 @@ public class Event {
     private String eventId;
     private String eventType;
     private String timestamp;
-    private Detail details;
-
-    public static class Detail {
-        private String userId;
-        private String ipAddress;
-    }
+    private String userId;
+    private String ipAddress;
 }
